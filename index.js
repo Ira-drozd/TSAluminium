8071ca012c4f74c28e7c2ca0c18b6a51a3ab3795
Ira <moby.dick.killer@gmail.com>
2020-06-17 11:15:33 +0300

    correct send for email

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const exphbs = require('express-handlebars');
 
-const pagesRoutes = require('./routes/pages');
-const mailer = require("./nodemailer/nodemailer")
+const pagesRoutes = require('./routes/pages');//
+const nodemailer = require('./nodemailer/index');//
 
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+//const urlencodedParser = bodyParser.urlencoded({extended: false});
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
@@ -18,49 +18,9 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);//для рендеринга страниц
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-app.use(pagesRoutes);
+app.use(pagesRoutes);//
+app.use(nodemailer);//
 app.use(express.static("public"));
-
-app.post("/contacts", urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400);
-
-    console.log(request.body)
-
-    function check() {
-        let tel = request.body.tel;
-        let email = request.body.email;
-        let message = request.body.message;
-
-        if (!tel || !email || !message) return {status: 2, message: "Еmpty fields"}
-
-        const sendMessage = {
-            to: `TSAluminium@yandex.by`,
-            subject: `Сообщение от ${request.body.email}`, // Subject line
-            //text: `Hello, this is just text ${request.body.email} ${request.body.password}`, // plain text body
-            html: `
-            <table><tbody>
-    <tr style="background-color: #f8f8f8;">
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Телефон:</b></td>
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'>${request.body.tel}</td>
-    </tr>
-    <tr style="background-color: #f8f8f8;">
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Email:</b></td>
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'>${request.body.email}</td>
-    </tr>
-    <tr style="background-color: #f8f8f8;">
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Сообщение:</b></td>
-        <td style='padding: 10px; border: #e9e9e9 1px solid;'>${request.body.message}</td>
-    </tr></tbody>
-            </table>
-            `}
-
-        mailer(sendMessage);
-
-        return {status: 1, message: "ok"}
-
-    }
-    response.send(check());
-});
 
 
 app.listen(PORT, function () {
--- public/js/mailer.js
+++ public/js/mailer.js
@@ -1,36 +1,36 @@
-$(document).ready(function () {
-
-    let formMail = document.forms.sendEmail;
-    let tel = formMail.elements.tel;
-    let email = formMail.elements.email;
-    let message = formMail.elements.message;
-    let toastHTML = '<span>Заполните все поля!</span>';
+$(function () {
 
     $('.modal').modal();
 
-     function submitForm(event) {
-         event.preventDefault();
-         $.ajax({
-             method: "POST",
-             url: "/contacts",
-             data: {tel: tel.value, email: email.value, message: message.value}
-         })
-             .done(function (data) {
-                 if (data.status === 1) {
-                     $('#modal1').modal('open');
-                     formMail.reset();
+    $('#send-button-email').on('click', function (e) {
+
+
+        e.preventDefault();
+        let data = {
+            tel: $('#send-tel').val(),
+            email: $('#send-email').val(),
+            message: $('#sens-message').val()
+        };
+        console.log(data)
+
+        $.ajax({
+            type: 'POST',
+            data: data,
+            url: '/contacts'
+        }).done(function (data) {
+            console.log(data)
+            if (data.status === 1) {
+                $('#modal1').modal('open');
+                $('#send-form-email')[0].reset();
+            }
+            if (data.status === 2) {
+                M.toast({html: toastHTML});
                  }
-                 if (data.status === 2) {
-                     M.toast({html: toastHTML});
-                 }
+        }).fail(function (data) {
+            console.log("Error load");
+            $('#modal2').modal('open');
+        });
 
-             })
-             .fail(function (data) {
-                 $('#modal2').modal('open');
-             })
-     }
 
-    formMail.addEventListener("submit", submitForm);
-
+    });
 });
-
